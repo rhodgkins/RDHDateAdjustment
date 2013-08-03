1f1bc66a10fecc09c983934a10a5c57ca2795900
Rich Hodgkins <rhodgkins@gmail.com>
2013-08-03 12:02:51 +0100

    Initial Commit

---
+++ RDHDateAdjustment/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RDHDateAdjustment
+//
+//  Created by Richard Hodgkins on 03/08/2013.
+//  Copyright (c) 2013 Rich H. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
